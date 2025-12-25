ac3ec20421dbdee01c0f4aedb8d5216a4f0158d6
mukuldeep maiti <mukul0018@gmail.com>
2025-12-25 17:42:13 +0530

    sorting

---
+++ cpJava/leet_MaximizeHappinessofSelectedChildren.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+
+        long ans = 0;
+        for(int i=0; i<k; i++){
+            ans += Math.max(0,happiness[happiness.length-1-i]-i);
+        }
+
+        return ans;
+        
+    }
+}
