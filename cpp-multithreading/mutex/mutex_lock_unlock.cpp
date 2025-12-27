873adcc54d8bbe945bc657620e0a48002dfc2160
mukuldeep maiti <mukul0018@gmail.com>
2025-12-27 16:02:47 +0530

    cmake

--- cpp-multithreading/mutex/mutex_lock_unlock.cpp
+++ cpp-multithreading/mutex/mutex_lock_unlock.cpp
@@ -1,4 +1,34 @@
 //
-// Created by mukul on 29-01-2026.
+// Created by mukul on 27-12-2025.
 //
 
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex mtx;
+int counter = 0; // Shared resource
+
+void increment(int iterations) {
+    for (int i = 0; i < iterations; ++i) {
+        mtx.lock();
+        counter++; // RACE CONDITION HAPPENS HERE
+        mtx.unlock();
+    }
+}
+
+int main() {
+    int iterations = 100000;
+
+    // Launch two threads to do the same task
+    std::thread t1(increment, iterations);
+    std::thread t2(increment, iterations);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Expected: " << iterations * 2 << std::endl;
+    std::cout << "Actual:   " << counter << std::endl;
+
+    return 0;
+}
