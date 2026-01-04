4e555383073ed44d976344d0ec2df88dac43ad10
mukuldeep maiti <mukul0018@gmail.com>
2026-01-04 15:12:25 +0530

    deadlock (two thread in circular wait)

---
+++ cpp-multithreading/mutex/circular_wait_deadlock.cpp
@@ -0,0 +1,35 @@
+//
+// Created by mukul on 04-01-2026.
+//
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex m1;
+std::mutex m2;
+
+void threadA() {
+    std::lock_guard<std::mutex> lock1(m1);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    std::lock_guard<std::mutex> lock2(m2);  // waits forever
+    std::cout << "Thread A acquired both\n";
+}
+
+void threadB() {
+    std::lock_guard<std::mutex> lock1(m2);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    std::lock_guard<std::mutex> lock2(m1);  // waits forever
+    std::cout << "Thread B acquired both\n";
+}
+
+int main() {
+    std::thread t1(threadA);
+    std::thread t2(threadB);
+
+    t1.join();
+    t2.join();
+}
+
