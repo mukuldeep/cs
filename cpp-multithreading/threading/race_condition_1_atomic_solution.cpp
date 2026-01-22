02a639bd1069e82f73bfcfcd3e050df62226c585
mukuldeep maiti <mukul0018@gmail.com>
2026-01-22 09:59:44 +0530

    atomic

---
+++ cpp-multithreading/threading/race_condition_1_atomic_solution.cpp
@@ -0,0 +1,31 @@
+//
+// Created by mukul on 22-12-2025.
+//
+
+#include <iostream>
+#include <thread>
+#include <atomic>
+std::atomic<int> counter(0);// Shared resource
+
+
+void increment(int iterations) {
+    for (int i = 0; i < iterations; ++i) {
+        counter++; // RACE CONDITION HAPPENS HERE
+    }
+}
+
+int main() {
+    int iterations = 100000;
+
+    // Launch two threads to do the same task
+    std::thread t1(increment, iterations);
+    std::thread t2(increment, iterations);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Expected: " << iterations * 2 << std::endl;
+    std::cout << "Actual:   " << counter << std::endl;
+
+    return 0;
+}
