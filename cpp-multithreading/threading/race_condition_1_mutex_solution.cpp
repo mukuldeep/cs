1bbff884628d505942e7dbff0d634814044cc81c
mukuldeep maiti <mukul0018@gmail.com>
2026-01-22 10:00:19 +0530

    mutex

---
+++ cpp-multithreading/threading/race_condition_1_mutex_solution.cpp
@@ -0,0 +1,33 @@
+//
+// Created by mukul on 22-12-202e5.
+//
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex mtx;
+int counter = 0; // Shared resource
+
+void increment(int iterations) {
+    for (int i = 0; i < iterations; ++i) {
+        std::lock_guard<std::mutex> lock(mtx);
+        counter++; // RACE CONDITION HAPPENS HERE
+    }
+}
+
+int main() {
+    int iterations = 100000;
+
+    // Launch two threads to do the same task
+    std::thread t1(increment, iterations);
+    std::thread t2(increment, iterations);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Expected: " << iterations * 2 << std::endl;
+    std::cout << "Actual:   " << counter << std::endl;
+
+    return 0;
+}
