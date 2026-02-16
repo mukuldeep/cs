cb35124e16b72682a6192b4faa949ce2be4b274f
mukuldeep maiti <mukul0018@gmail.com>
2026-02-16 19:40:16 +0530

    bitm

---
+++ cp/leetcode/190_Reverse_Bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        unsigned int x = n;
+        x = ((x >> 1) & 0x55555555) | ((x & 0x55555555) << 1);
+        x = ((x >> 2) & 0x33333333) | ((x & 0x33333333) << 2);
+        x = ((x >> 4) & 0x0F0F0F0F) | ((x & 0x0F0F0F0F) << 4);
+        x = ((x >> 8) & 0x00FF00FF) | ((x & 0x00FF00FF) << 8);
+        x = ((x >> 16)) | (x << 16);
+        return (int)x;
+    }
+};
